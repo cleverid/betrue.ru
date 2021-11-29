1bc5a0510f0363ace3ae983c0aeb39051082d8eb
Шапошников Евгений <eawapownikov@gmail.com>
2021-11-29 22:29:44 +0300

    Create

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# betrue.ru
-Site betrue.ru
+## Getting Started
+
+First, run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage, NextPageContext } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export async function getStaticProps(context: NextPageContext) {
+  return {
+    props: {},
+  };
+}
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>BeTrue - кастомная разработка</title>
+        <meta name="description" content="Кастомная разработка web сервисов" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>.beTrue()</h1>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+};
+
+export default Home;
